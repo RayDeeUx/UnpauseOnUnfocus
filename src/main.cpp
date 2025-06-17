5019738bacd8b6744c2cc6527e17a1b1014a91be
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2025-06-17 08:36:02 -0400

    is this enough linespacing for you @dankmeme01

--- src/main.cpp
+++ src/main.cpp
@@ -7,18 +7,25 @@ bool closeAllFLAlertLayers = false;
 
 class $modify(MyAppDelegate, AppDelegate) {
 	static PauseLayer* getPauseLayer() {
-		return CCScene::get()->getChildByType<PauseLayer>(0);
+		const auto scene = CCScene::get();
+		if (scene) return scene->getChildByType<PauseLayer>(0);
+		return nullptr;
 	}
 	static void unpauseOnUnfocusResumeGame() {
 		if (!enabled) return;
+
 		const auto scene = CCScene::get();
+		if (!scene || scene->getChildrenCount() < 1) return;
+
 		const auto pauseLayer = getPauseLayer();
-		if (!pauseLayer || !scene || scene->getChildrenCount() < 1) return;
+		if (!pauseLayer) return;
+
 		if (closeAllFLAlertLayers && scene->getChildren()) {
 			for (const auto fooBar : CCArrayExt<CCObject*>(scene->getChildren())) {
 				if (const auto isAlert = typeinfo_cast<FLAlertLayer*>(fooBar)) isAlert->removeMeAndCleanup();
 			}
 		}
+
 		return pauseLayer->onResume(nullptr);
 	}
 	void applicationWillResignActive() {
