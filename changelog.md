a0ae729ae471c1a39eac5875767cfca9ae703406
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2025-06-17 08:06:45 -0400

    Is It Cake? (2022)

--- changelog.md
+++ changelog.md
@@ -1,4 +1,7 @@
 # UnpauseOnUnfocus Changelog
+## v1.0.3
+- Added an option to auto-close all `FLAlertLayer`s before unpausing.
+- By the time you're reading this, the mod's logo should be updated as well.
 ## v1.0.2
 - Fix a bug where all PauseLayers would be closed regardless of reason. Thank you Capeling!
   - This *should* hopefully be a foolproof fix, but only time will tell.
--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,44 @@
 
 using namespace geode::prelude;
 
+bool enabled = false;
+bool closeAllFLAlertLayers = false;
+
 class $modify(MyAppDelegate, AppDelegate) {
 	static PauseLayer* getPauseLayer() {
-		return CCDirector::get()->getRunningScene()->getChildByType<PauseLayer>(0);
+		return CCScene::get()->getChildByType<PauseLayer>(0);
+	}
+	static void unpauseOnUnfocusResumeGame() {
+		if (!enabled) return;
+		const auto CCScene = CCScene::get();
+		const auto pauseLayer = getPauseLayer();
+		if (!pauseLayer || !scene || scene->getChildrenCount() < 1) return;
+		if (closeAllFLAlertLayers && scene->getChildren()) {
+			for (const auto fooBar : CCArrayExt<CCObject*>(scene->getChildren())) {
+				if (const auto isAlert = typeinfo_cast<FLAlertLayer*>(fooBar)) isAlert->removeMeAndCleanup();
+			}
+		}
+		return pauseLayer->onResume(nullptr);
 	}
 	void applicationWillResignActive() {
 		if (getPauseLayer()) return AppDelegate::applicationWillResignActive();
 		AppDelegate::applicationWillResignActive();
-		if (!Mod::get()->getSettingValue<bool>("enabled")) return;
-		if (const auto pauseLayer = getPauseLayer()) return pauseLayer->onResume(nullptr);
+		unpauseOnUnfocusResumeGame();
 	}
 	void applicationDidEnterBackground() {
 		if (getPauseLayer()) return AppDelegate::applicationWillResignActive();
 		AppDelegate::applicationDidEnterBackground();
-		if (!Mod::get()->getSettingValue<bool>("enabled")) return;
-		if (const auto pauseLayer = getPauseLayer()) return pauseLayer->onResume(nullptr);
+		unpauseOnUnfocusResumeGame();
 	}
 };
+
+$on_mod(Loaded) {
+	enabled = Mod::get()->getSettingValue<bool>("enabled");
+	closeAllFLAlertLayers = Mod::get()->getSettingValue<bool>("closeAllFLAlertLayers");
+	listenForSettingChanges<bool>("enabled", [](bool newEnabled) {
+		enabled = newEnabled;
+	});
+	listenForSettingChanges<bool>("closeAllFLAlertLayers", [](bool newCloseAllFLAlertLayers) {
+		closeAllFLAlertLayers = newCloseAllFLAlertLayers;
+	});
+}
